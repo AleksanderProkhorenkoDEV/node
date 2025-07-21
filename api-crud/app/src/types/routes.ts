f7e227d736cfec7d29920058da2b053db4365aeb
aleksander <aprokhorenkodev@gmail.com>
2025-07-21 18:36:12 +0200

    feat: create a ts file to request handlers

---
+++ api-crud/app/src/types/routes.ts
@@ -0,0 +1,17 @@
+import { IncomingMessage, ServerResponse } from "node:http";
+
+export type HttpMethod = "GET" | "POST" | "PUT";
+export type RequestHandler = GetRequest | PostRequest;
+
+type GetHandleParams = {
+  res: ServerResponse;
+  params?: URLSearchParams;
+};
+
+type PostHandleParams = {
+  res: ServerResponse;
+  req: IncomingMessage;
+};
+
+export type GetRequest = (args: GetHandleParams) => void;
+export type PostRequest = (args: PostHandleParams) => void;
