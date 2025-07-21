38ee2526da836730a2e5e0234a75dd792ae498b0
aleksander <aprokhorenkodev@gmail.com>
2025-07-21 18:35:22 +0200

    feat: create function to get the patter of route and function to get the dynamic params and query params to pass the controller

--- api-crud/app/src/server/router.ts
+++ api-crud/app/src/server/router.ts
@@ -1,16 +1,10 @@
-import { IncomingMessage, ServerResponse } from "node:http";
-
-export type HttpMethod = "GET" | "POST" | "PUT";
-export type RequestHandler = (
-  req: IncomingMessage,
-  res: ServerResponse
-) => void;
+import { HttpMethod, RequestHandler } from "../types/routes";
+import { URL } from "node:url";
 
 interface Route {
   method: HttpMethod;
-  pathPattern: string;
-  regex: RegExp;
-  paramNames: string[];
+  pattern: RegExp;
+  path: string;
   handler: RequestHandler;
 }
 
@@ -18,11 +12,44 @@ export class Router {
   private routes: Route[] = [];
 
   save(method: HttpMethod, path: string, handler: RequestHandler) {
-    this.convertToPattern(path);
+    const pattern = this.convertToPattern(path);
+    this.routes.push({ method, pattern, path, handler });
   }
 
   convertToPattern(originalPath: string) {
-    const dividePath = originalPath.split("/");
-    console.log(dividePath);
+    return new RegExp(
+      `^${originalPath.replace(/\//g, "\\/").replace(/\:\w+/g, "([^/]+)")}$`
+    );
+  }
+
+  searchRoute(method: HttpMethod, path: string): undefined | Route {
+    const routesFilterByMethod = this.routes.filter((r) => r.method === method);
+
+    const route = routesFilterByMethod.find((route) =>
+      path.match(route.pattern)
+    );
+
+    if (!route) return undefined;
+
+    return route;
+  }
+
+  extractParams(url: URL, route: Route) {
+    let params: Record<string, string | number | boolean> = {};
+    const pathParts = url.pathname.match(route.pattern).filter(Boolean);
+    const dinamicKeys = route.path.match(route.pattern).filter(Boolean);
+
+    //Get the key and value of dynamic params in route (example: /user/:id)
+    for (let i = 1; i < pathParts.length && i < dinamicKeys.length; i++) {
+      params[dinamicKeys[i].replace(/\:/, "")] = pathParts[i];
+    }
+
+    //Get the query param of the url (example: /user?name=Aleksander)
+    for (const [key, value] of url.searchParams.entries()) {
+      params[key] = value;
+    }
+    console.log(params);
+    
+    return params;
   }
 }
