69ebc3f3cbae78f2dca2e4beb156de49cc25cc9d
aleksander <aprokhorenkodev@gmail.com>
2025-07-25 18:26:50 +0200

    feat: create a function to desacoplate http controller to the repository

--- api-crud/app/src/utils/helpers.ts
+++ api-crud/app/src/utils/helpers.ts
@@ -1,3 +1,17 @@
+import { UserQueryParams } from "../types/user";
+
 export const sleep = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const parseParamsUser = (
+  params: Record<string, string>
+): UserQueryParams | null => {
+  if (Object.keys(params).length === 0) return null;
+  return {
+    id: parseInt(params?.id),
+    name: params?.name,
+    lastName: params?.lastName,
+    email: params?.email,
+  };
 };
