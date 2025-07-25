745545ccddc98ae7b6539758ba9f151bd19ed915
aleksander <aprokhorenkodev@gmail.com>
2025-07-25 18:26:01 +0200

    feat: add filter in findAll method and findUser method

--- api-crud/app/src/repository/userRepository.ts
+++ api-crud/app/src/repository/userRepository.ts
@@ -1,14 +1,28 @@
 import { User } from "../entities/Users";
-import { IUser } from "../types/user";
+import { IUser, UserQueryParams } from "../types/user";
 import { sleep } from "../utils/helpers";
 
 export interface IUserRepository {
   save(user: User): Promise<void>;
-  findAll(): Promise<User[]>;
+  findAll(queryParams: UserQueryParams): Promise<User[]>;
+  findUser(id: string): Promise<User>;
 }
 
 export class UserRepository implements IUserRepository {
-  private users: IUser[] = [];
+  private users: IUser[] = [
+    {
+      id: "1",
+      name: "Aleksander",
+      lastName: "Trujillo Prokhorenko",
+      email: "aleksander@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Aleksander",
+      lastName: "Trujillo Prokhorenko",
+      email: "prokhorenko@gmail.com",
+    },
+  ];
 
   async save(user: User): Promise<void> {
     if (!user.isValid()) throw new Error("User entity isn't valid");
@@ -16,9 +30,26 @@ export class UserRepository implements IUserRepository {
     await sleep(1500);
   }
 
-  async findAll(): Promise<User[]> {
-    const usersRaw = this.users;
+  async findAll(queryParams: UserQueryParams): Promise<User[]> {    
+    if (!queryParams) {
+      return this.users.map((userRaw) => User.fromJson(userRaw));
+    }
+
+    const filterUsers = this.users.filter((user) => {
+      if (queryParams.name !== undefined && queryParams.name.toLowerCase !== user.name.toLowerCase) return false;
+      if (queryParams.lastName !== undefined && queryParams.lastName.toLowerCase !== user.lastName.toLowerCase) return false;
+      if (queryParams.email !== undefined && queryParams.email !== user.email) return false;
+
+      return true;
+    });
+
     await sleep(1000);
-    return usersRaw.map((userRaw) => User.fromJson(userRaw));
+    return filterUsers.map((userRaw) => User.fromJson(userRaw));
+  }
+
+  async findUser(id: string): Promise<User> {
+    const userRaw = this.users.find((user) => user.id === id);
+    await sleep(1000);
+    return User.fromJson(userRaw);
   }
 }
