b3c4eafad7df9a52d395e474f61facd270db8b6e
aleksander <aprokhorenkodev@gmail.com>
2025-07-25 18:21:59 +0200

    feat: register new route

--- api-crud/app/src/index.ts
+++ api-crud/app/src/index.ts
@@ -1,15 +1,20 @@
-import { createUser, listUser, updateUser } from "./http/userController";
+import {
+  createUser,
+  listUser,
+  updateUser,
+  findUser,
+} from "./http/userController";
 import { Server } from "./server/server";
 
 const server = new Server();
 
 server.addRoutes("GET", "/list-users", listUser);
 
+server.addRoutes("GET", "/user/:id", findUser);
+
 server.addRoutes("POST", "/create-user", createUser);
 
 server.addRoutes("PUT", "/update-user/:id", updateUser);
-
-server.addRoutes("GET", "/user/:id/data/:name", (res) => {res.end()});
 
 server.start(3000, () => {
   console.log("Server running on port 3000");
