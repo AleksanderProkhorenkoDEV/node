134d5bb91e3f79cfdb235dade6091af2bbc85256
aleksander <aprokhorenkodev@gmail.com>
2025-07-29 17:10:22 +0200

    feat: create the function

--- node-jest/app/src/index.ts
+++ node-jest/app/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Funciona');
+export function sum(firstNumber: number, secondNumber: number): number {
+  return firstNumber + secondNumber;
+}
+
+export function div(divisor: number, dividendo: number): number {
+  if (dividendo === 0) throw new Error("Cannot be divided by zero");
+  return divisor / dividendo;
+}
