8e9a8991a79b23623d708d65c3ae7904aae9f0be
aleksander <aprokhorenkodev@gmail.com>
2025-08-08 12:42:31 +0200

    style: formate code

--- learnt-eslint/app/src/index.ts
+++ learnt-eslint/app/src/index.ts
@@ -1,7 +1,7 @@
-console.log("hello eslont, u have a warn");
+console.log("hello eslont, u have a warn")
 
-const fruits: string[] = ["apple", "watermelon", "banana", "kiwi"];
+const fruits: string[] = ["apple", "watermelon", "banana", "kiwi"]
 
 fruits.forEach((fruit) => {
-  console.log(fruit);
-});
+	console.log(fruit)
+})
